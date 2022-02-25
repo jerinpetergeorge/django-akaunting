a5f8f6861c0b612515aba75df17199aba6cd52f7
Jerin Peter George <jerinpetergeorge@gmail.com>
2022-02-26 01:24:01 +0530

    Auto-save User from request

--- purchases/payments/admin.py
+++ purchases/payments/admin.py
@@ -7,10 +7,16 @@ from .models import Payment
 class PaymentAdmin(admin.ModelAdmin):
     list_display = (
         "id",
-        "user",
+        "item",
         "amount",
         "date",
         "vendor",
         "category",
         "payment_method",
+        "user",
     )
+    readonly_fields = ["user"]
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
