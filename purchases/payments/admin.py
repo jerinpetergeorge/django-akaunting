03d5e1e34f5a1d5ad2c3f89bf453bcc844daf89e
Jerin Peter George <jerinpetergeorge@gmail.com>
2022-02-27 13:18:48 +0530

    Update Payment Admin with file URL

--- purchases/payments/admin.py
+++ purchases/payments/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Payment
 
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display_links = ("id", "item")
+    list_display_links = ("id", "item", "file")
     list_display = (
         "id",
         "item",
@@ -14,10 +15,22 @@ class PaymentAdmin(admin.ModelAdmin):
         "vendor",
         "category",
         "payment_method",
-        "user",
+        "file",
     )
     readonly_fields = ["user"]
     list_filter = ["category", "vendor", "payment_method"]
+
+    def file(self, obj: Payment):
+        if not obj.attachment:
+            return
+        return format_html(
+            f"<a href='{obj.attachment.url}'  "
+            f"target='_blank' >{obj.attachment.name}</a>"
+        )
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request=request)
+        return qs.filter(user=request.user)
 
     def save_model(self, request, obj, form, change):
         obj.user = request.user
